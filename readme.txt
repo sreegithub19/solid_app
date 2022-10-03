5ca5574166c5cbd969943e2cb54433b09705a877
K <sreedhar.k@rakuten.com>
2022-10-03 21:13:50 +0530

    Changes

---
+++ readme.txt
@@ -0,0 +1,6 @@
+1. npm install --force
+2. npm run dev
+3. npm i @solidjs/router --force
+
+Note:
+1. Refresh the URLs once navigated to puzzles/sudoku/chess.
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,50 @@
 import Counter from "~/components/Counter";
-import "./index.css";
 
 export default function Home() {
+    const css_ = `
+  body {
+  font-family: Gordita, Roboto, Oxygen, Ubuntu, Cantarell,
+    'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+main {
+  text-align: center;
+  padding: 1em;
+  margin: 0 auto;
+}
+
+h1 {
+  color: #335d92;
+  text-transform: uppercase;
+  font-size: 4rem;
+  font-weight: 100;
+  line-height: 1.1;
+  margin: 4rem auto;
+  max-width: 14rem;
+}
+
+p {
+  max-width: 14rem;
+  margin: 2rem auto;
+  line-height: 1.35;
+}
+
+@media (min-width: 480px) {
+  h1 {
+    max-width: none;
+  }
+
+  p {
+    max-width: none;
+  }
+}
+
+  `;
+
+
   return (
     <main>
+      <style>{css_}</style>
       <h1>Hello world!</h1>
       <Counter />
       <p>
